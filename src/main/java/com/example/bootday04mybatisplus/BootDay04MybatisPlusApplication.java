1c9c7939224e609d0f4ac3cadd13f3e8c948fc11
dinghaibing <357255054@qq.com>
2022-07-12 16:30:51 +0800

    mybatisPlus second commit

--- src/main/java/com/example/bootday04mybatisplus/BootDay04MybatisPlusApplication.java
+++ src/main/java/com/example/bootday04mybatisplus/BootDay04MybatisPlusApplication.java
@@ -9,6 +9,7 @@ public class BootDay04MybatisPlusApplication {
     public static void main(String[] args) {
         SpringApplication.run(BootDay04MybatisPlusApplication.class, args);
         System.out.println("hello,git!");
+        System.out.println("hello,git2!");
     }
 
 }
